51779ad89b9018950d459ff4defbf91d397cf455
정훈 <51181222+lionleeee@users.noreply.github.com>
2023-08-08 22:16:32 +0900

    Time: 63 ms (78.03%), Space: 42.2 MB (72.90%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,10 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-      let map = {};
-  for (let i = 0; i < nums.length; i++) {
-    let res = target - nums[i];
-    if (res in map) return [i, map[res]];
-    map[nums[i]] = i;
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let res = target - nums[i];
+        if (res in map) return [i, map[res]];
+        map[nums[i]] = i;
   }
 };
