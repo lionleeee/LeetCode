9103cbe38ea4a7e1f62736a08f1d5af2b1154c7b
정훈 <51181222+lionleeee@users.noreply.github.com>
2023-08-10 21:25:15 +0900

    Time: 161 ms (73.28%), Space: 51.7 MB (17.56%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -7,8 +7,7 @@ var isPalindrome = function(x) {
     const MaxNum = 2147483647;  //2^31-1 값
     
     if( x >= 0){
-         var numStr = x.toString();
-        var reverse = numStr.split('').reverse().join('');
+        var reverse = x.toString().split('').reverse().join('');
         return x === Number(reverse);
     }
     return false;
