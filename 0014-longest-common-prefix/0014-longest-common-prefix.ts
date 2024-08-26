7a56bbfe67cdbc81b79eea155e97fa03dfc30f6f
정훈 <51181222+lionleeee@users.noreply.github.com>
2024-08-26 20:13:48 +0900

    Time: 61 ms (56.01%), Space: 51.7 MB (72.45%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.ts
@@ -0,0 +1,12 @@
+function longestCommonPrefix(strs: string[]): string {
+    
+    let prefix = strs[0];
+    for(let word of strs){
+        while(word.indexOf(prefix) != 0){
+            prefix = prefix.substring(0, prefix.length -1);
+        }
+    }
+    return prefix
+    
+    
+};
